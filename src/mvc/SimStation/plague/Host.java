dbeb75f00e740fd01885c6996be662d44883f9eb
Jspikes <42878739+Jspikes@users.noreply.github.com>
2024-04-17 23:50:00 -0700

    fixes and refactoring

--- src/mvc/SimStation/plague/Host.java
+++ src/mvc/SimStation/plague/Host.java
@@ -23,7 +23,7 @@ public class Host extends Agent {
         xc = Utilities.rng.nextInt(10, FRAME_WIDTH - 10);
         yc = Utilities.rng.nextInt(10, FRAME_HEIGHT - 10);
     }
-    // optional code to smooth the graphics a little
+    // optional override to smooth the graphics a little
 //    public void run() {
 //        if(!running) {
 //            running = true;
@@ -37,7 +37,7 @@ public class Host extends Agent {
     @Override
     public void update() {
         heading = Heading.random();
-        int steps = Utilities.rng.nextInt(20) + 1;
+        int steps = Utilities.rng.nextInt(5) + 1;
         move(steps);
         Host p = (Host) world.getNeighbor(this, world.RANGE);
         if(p == null) {return;}
@@ -48,50 +48,52 @@ public class Host extends Agent {
         }
     }
 
-    public void move(int steps) {
-        for (int i = 0; i < steps; i++) {
-            checkSuspended();
-            switch(heading) {
-                case NORTH: {
-                    yc++;
-                    break;
-                }
-                case SOUTH: {
-                    yc--;
-                    break;
-                }
-                case EAST: {
-                    xc++;
-                    break;
-                }
-                case WEST: {
-                    xc--;
-                    break;
-                }
-            }
-            yc = Math.floorMod(yc, FRAME_HEIGHT);
-            xc = Math.floorMod(xc, FRAME_WIDTH);
-            world.changed(); // Notify the world of the change in position
-            try{
-                Thread.sleep(20);
-            } catch(InterruptedException e) {
-                System.out.println(e);
-            }
-        }
-    }
+//    smoother animating move method
+
+//    public void move(int steps) {
+//        for (int i = 0; i < steps; i++) {
+//            checkSuspended();
+//            switch(heading) {
+//                case NORTH: {
+//                    yc++;
+//                    break;
+//                }
+//                case SOUTH: {
+//                    yc--;
+//                    break;
+//                }
+//                case EAST: {
+//                    xc++;
+//                    break;
+//                }
+//                case WEST: {
+//                    xc--;
+//                    break;
+//                }
+//            }
+//            yc = Math.floorMod(yc, FRAME_HEIGHT);
+//            xc = Math.floorMod(xc, FRAME_WIDTH);
+//            world.changed(); // Notify the world of the change in position
+//            try{
+//                Thread.sleep(20);
+//            } catch(InterruptedException e) {
+//                System.out.println(e);
+//            }
+//        }
+//    }
 
     private int d100(){
         return (int) Math.ceil(Math.random() * 100);
     }
-
-    private synchronized void checkSuspended() {
-        try {
-            while(!stopped && suspended) {
-                wait();
-                suspended = false;
-            }
-        } catch (InterruptedException e) {
-            System.out.println(e);
-        }
-    }
+//
+//    private synchronized void checkSuspended() {
+//        try {
+//            while(!stopped && suspended) {
+//                wait();
+//                suspended = false;
+//            }
+//        } catch (InterruptedException e) {
+//            System.out.println(e);
+//        }
+//    }
 }
